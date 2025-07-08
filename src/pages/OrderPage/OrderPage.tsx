e6808a94f49243aa0b4f51449ec9bd1f3fee3688
Ajin <ajin@Ajinui-noteubug.local>
2025-07-08 19:14:31 +0800

    주문페이지 레이아웃 적용

--- src/pages/OrderPage/OrderPage.tsx
+++ src/pages/OrderPage/OrderPage.tsx
@@ -4,6 +4,7 @@ import { messageCards } from '@/data/messageCards';
 import MessageCardSelector, { type MessageCard } from './MessageCardSelector';
 import SelectedCardPreview from './SelectedCardPreview';
 import { useState } from 'react';
+import Layout from '@/components/Layout';
 
 const OrderPage = () => {
   const { id } = useParams();
@@ -17,17 +18,15 @@ const OrderPage = () => {
   };
 
   return (
-    <div>
-      <NavigationBar/>
+    <Layout>
+      <NavigationBar />
       <MessageCardSelector cards={messageCards} selectedId={selectedCard?.id ?? null}
-        onSelect={handleSelectCard}/>
-        {selectedCard && (
-        <>
-          <SelectedCardPreview card={selectedCard} message={message} onChange={setMessage} />
-        </>
+        onSelect={handleSelectCard} />
+      {selectedCard && (
+        <SelectedCardPreview card={selectedCard} message={message} onChange={setMessage} />
       )}
       <p>선택한 상품 ID: {id}</p>
-    </div>
+    </Layout>
   );
 };
 
