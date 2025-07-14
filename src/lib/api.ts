96c610a2c5ec9cd0b04ba7c46934d73c2c250018
Jaeho <sjaeho348@gmail.com>
2025-07-15 02:13:30 +0900

    feat : Ranking API함수, 타입 정의

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { type Theme } from "../types/api";
+import { type Theme, type TargetType, type RankType, type RankingProduct} from "../types/api";
 
 export const api = axios.create({
     baseURL: import.meta.env.VITE_API_BASE_URL
@@ -8,6 +8,20 @@ export const api = axios.create({
 export const getThemes = async (): Promise<Theme[]> => {
     try {
         const response = await api.get<{data: Theme[]}>("/themes");
+        return response.data.data;
+    } catch (error) {
+        throw error;
+    }
+}
+
+export const getRankingProducts = async (targetType: TargetType = 'ALL', rankType: RankType = 'MANY_WISH'): Promise<RankingProduct[]> => {
+    try {
+        const response = await api.get<{data: RankingProduct[]}>("/products/ranking", {
+            params: {
+                targetType,
+                rankType
+            }
+        });
         return response.data.data;
     } catch (error) {
         throw error;
