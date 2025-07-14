b77c1f7150c372cc35ed846702b34bb01b6f3fa8
Taewoong Heo <chamjoeun0111@gmail.com>
2025-07-15 02:17:06 +0900

    feat: 선물 테마 목록 api 요청 추가
    - 기존 mock 데이터를 api 데이터로 변경

--- src/pages/HomePage/components/Category/Category.tsx
+++ src/pages/HomePage/components/Category/Category.tsx
@@ -1,17 +1,48 @@
-import { categoryMockData } from '@/data/categoryMockData';
-import ThemeCard from './ThemeCard';
-import { CATEGORY_LABELS } from './constants/labels';
-import { GiftThemeSection, SectionHeader, SectionTitle, ThemeGrid } from './Category.styles';
+import ThemeCard from "./ThemeCard";
+import { CATEGORY_LABELS } from "./constants/labels";
+import {
+  GiftThemeSection,
+  SectionHeader,
+  SectionTitle,
+  ThemeGrid,
+} from "./Category.styles";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+interface GiftTheme {
+  themeId: number;
+  name: string;
+  image: string;
+}
 
 function Category() {
+  const [giftThemes, setGiftThemes] = useState<GiftTheme[]>([]);
+
+  useEffect(() => {
+    const fetchGiftThemes = async () => {
+      try {
+        const response = await axios.get("http://localhost:3000/api/themes");
+        setGiftThemes(response.data.data);
+      } catch (error) {
+        console.error("선물 테마 데이터 조회 실패", error);
+      }
+    };
+
+    fetchGiftThemes();
+  }, []);
+
   return (
     <GiftThemeSection>
       <SectionHeader>
         <SectionTitle>{CATEGORY_LABELS.SECTION_TITLE}</SectionTitle>
       </SectionHeader>
       <ThemeGrid>
-        {categoryMockData.map((theme) => (
-          <ThemeCard key={theme.themeId} name={theme.name} image={theme.image} />
+        {giftThemes.map((theme) => (
+          <ThemeCard
+            key={theme.themeId}
+            name={theme.name}
+            image={theme.image}
+          />
         ))}
       </ThemeGrid>
     </GiftThemeSection>
