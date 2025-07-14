b1bc4feed8c5291a71add20d8b85fef8c51c1855
Taewoong Heo <chamjoeun0111@gmail.com>
2025-07-15 02:38:41 +0900

    feat(Category): api 요청 시 로딩 UI 추가

---
+++ src/pages/HomePage/components/Category/Category.styles.ts
@@ -0,0 +1,65 @@
+import styled from "@emotion/styled";
+
+export const GiftThemeSection = styled.section`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+export const SectionHeader = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+
+  width: 100%;
+  box-sizing: border-box;
+
+  padding-left: ${({ theme }) => theme.spacing[4]};
+`;
+
+export const SectionTitle = styled.h2`
+  font-size: ${({ theme }) => theme.typography.title.title1Bold.fontSize};
+  font-weight: ${({ theme }) => theme.typography.title.title1Bold.fontWeight};
+`;
+
+export const ThemeGrid = styled.div`
+  display: grid;
+  grid-template-columns: ${({ theme }) => theme.layout.grid.columns.fixed5};
+  gap: ${({ theme }) => theme.layout.grid.gaps.sm};
+
+  margin-top: ${({ theme }) => theme.spacing[5]};
+  margin-bottom: ${({ theme }) => theme.spacing[5]};
+`;
+
+export const LoadingContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 124px;
+  margin-bottom: 124px;
+`;
+
+export const LoadingSpinner = styled.div`
+  width: ${({ theme }) => theme.spacing[10]};
+  height: ${({ theme }) => theme.spacing[10]};
+  border-top: ${({ theme }) => theme.spacing[1]} solid
+    ${({ theme }) => theme.colors.gray[800]};
+  border-bottom: ${({ theme }) => theme.spacing[1]} solid
+    ${({ theme }) => theme.colors.gray[800]};
+  border-left: ${({ theme }) => theme.spacing[1]} solid
+    ${({ theme }) => theme.colors.gray[0]};
+  border-right: ${({ theme }) => theme.spacing[1]} solid
+    ${({ theme }) => theme.colors.gray[0]};
+  border-radius: 50%;
+  animation: spin 1s linear infinite;
+
+  @keyframes spin {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+`;
--- src/pages/HomePage/components/Category/Category.tsx
+++ src/pages/HomePage/components/Category/Category.tsx
@@ -2,6 +2,8 @@ import ThemeCard from "./ThemeCard";
 import { CATEGORY_ERROR_MESSAGE, CATEGORY_LABELS } from "./constants/labels";
 import {
   GiftThemeSection,
+  LoadingContainer,
+  LoadingSpinner,
   SectionHeader,
   SectionTitle,
   ThemeGrid,
@@ -17,16 +19,22 @@ interface GiftTheme {
 
 function Category() {
   const [giftThemes, setGiftThemes] = useState<GiftTheme[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchGiftThemes = async () => {
       try {
+        // 로딩 테스트
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+
         const response = await axios.get(
           `${import.meta.env.VITE_API_BASE_URL}/api/themes`
         );
         setGiftThemes(response.data.data);
       } catch (error) {
         console.error(CATEGORY_ERROR_MESSAGE.FETCH_ERROR, error);
+      } finally {
+        setIsLoading(false);
       }
     };
 
@@ -38,15 +46,21 @@ function Category() {
       <SectionHeader>
         <SectionTitle>{CATEGORY_LABELS.SECTION_TITLE}</SectionTitle>
       </SectionHeader>
-      <ThemeGrid>
-        {giftThemes.map((theme) => (
-          <ThemeCard
-            key={theme.themeId}
-            name={theme.name}
-            image={theme.image}
-          />
-        ))}
-      </ThemeGrid>
+      {isLoading ? (
+        <LoadingContainer>
+          <LoadingSpinner />
+        </LoadingContainer>
+      ) : (
+        <ThemeGrid>
+          {giftThemes.map((theme) => (
+            <ThemeCard
+              key={theme.themeId}
+              name={theme.name}
+              image={theme.image}
+            />
+          ))}
+        </ThemeGrid>
+      )}
     </GiftThemeSection>
   );
 }
