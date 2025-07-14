ed6322af157fe2386e55eeeca9409d74eae73748
Moderator11 <p7925221@gmail.com>
2025-07-14 15:56:59 +0900

    feat: add api entries

---
+++ src/apis/BackEnd/apiEndPoints.ts
@@ -0,0 +1,60 @@
+const PROTOCOL = "http";
+const ENDPOINT = `localhost:3000`;
+
+const API_SECTIONS = {
+  LOGIN: `/api/login`,
+  THEME: `/api/themes`,
+  PRODUCT: `/api/products`,
+  ORDER: `/api/order`
+};
+
+const LOGIN_ENTRIES = (() => {
+  const BASE = `${PROTOCOL}://${ENDPOINT}/${API_SECTIONS.LOGIN}`;
+  return {
+    BASE
+  };
+})();
+
+const THEME_ENTRIES = (() => {
+  const BASE = `${PROTOCOL}://${ENDPOINT}/${API_SECTIONS.THEME}`;
+  return {
+    BASE,
+    DETAIL: (themeId: number) => `${BASE}/${themeId}`,
+    INFO: (themeId: number) => `${BASE}/${themeId}/info`,
+    PRODUCTS: (themeId: number) => `${BASE}/${themeId}/products`
+  };
+})();
+
+const PRODUCT_ENTRIES = (() => {
+  const BASE = `${PROTOCOL}://${ENDPOINT}/${API_SECTIONS.PRODUCT}`;
+  return {
+    BASE,
+    INFO: {
+      GENERAL: (productId: number) => `${BASE}/${productId}`,
+      DETAIL: (productId: number) => `${BASE}/${productId}/detail`
+    },
+    WISH: (productId: number) => `${BASE}/${productId}/wish`,
+    HIGHLIGHT_REVIEW: (productId: number) =>
+      `${BASE}/${productId}/highlight-review`,
+    SUMMARY: (productId: number) => `${BASE}/${productId}/summary`,
+    RANKING: `${BASE}/ranking`
+  };
+})();
+
+const ORDER_ENTRIES = (() => {
+  const BASE = `${PROTOCOL}://${ENDPOINT}/${API_SECTIONS.ORDER}`;
+  return {
+    BASE
+  };
+})();
+
+//Back-End api entries
+export const BE = {
+  PING: `/ping`,
+  API: {
+    LOGIN: LOGIN_ENTRIES,
+    THEME: THEME_ENTRIES,
+    PRODUCT: PRODUCT_ENTRIES,
+    ORDER: ORDER_ENTRIES
+  }
+};
