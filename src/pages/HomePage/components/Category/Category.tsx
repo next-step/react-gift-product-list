8cc793226c921910f4bc45ec6d67e51c0dee1275
Taewoong Heo <chamjoeun0111@gmail.com>
2025-07-15 12:06:53 +0900

    feat: 데이터 없거나 에러 발생 시 예외처리 UI 구현

--- src/pages/HomePage/components/Category/Category.tsx
+++ src/pages/HomePage/components/Category/Category.tsx
@@ -10,6 +10,7 @@ import {
 } from "./Category.styles";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import styled from "@emotion/styled";
 
 interface GiftTheme {
   themeId: number;
@@ -17,9 +18,25 @@ interface GiftTheme {
   image: string;
 }
 
+const ErrorContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  margin-top: 124px;
+  margin-bottom: 124px;
+`;
+
+const ErrorMessage = styled.p`
+  font-size: ${({ theme }) => theme.typography.label.label1Regular.fontSize};
+  font-weight: ${({ theme }) =>
+    theme.typography.label.label1Regular.fontWeight};
+`;
+
 function Category() {
   const [giftThemes, setGiftThemes] = useState<GiftTheme[]>([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
 
   useEffect(() => {
     const fetchGiftThemes = async () => {
@@ -27,9 +44,15 @@ function Category() {
         const response = await axios.get(
           `${import.meta.env.VITE_API_BASE_URL}/api/themes`
         );
-        setGiftThemes(response.data.data);
+        if (response.data.data.length === 0) {
+          setIsError(true);
+        } else {
+          setGiftThemes(response.data.data);
+          setIsError(false);
+        }
       } catch (error) {
         console.error(CATEGORY_ERROR_MESSAGE.FETCH_ERROR, error);
+        setIsError(true);
       } finally {
         setIsLoading(false);
       }
@@ -43,7 +66,11 @@ function Category() {
       <SectionHeader>
         <SectionTitle>{CATEGORY_LABELS.SECTION_TITLE}</SectionTitle>
       </SectionHeader>
-      {isLoading ? (
+      {isError ? (
+        <ErrorContainer>
+          <ErrorMessage>데이터를 불러오는 중 에러가 발생했어요</ErrorMessage>
+        </ErrorContainer>
+      ) : isLoading ? (
         <LoadingContainer>
           <LoadingSpinner />
         </LoadingContainer>
