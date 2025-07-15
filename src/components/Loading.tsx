bb0dca9bcc63321d9c45d720ae8ff5641051f6f8
Changmok LEE <cmlee5075@gmail.com>
2025-07-15 16:51:01 +0900

    feat: Loading 컴포넌트 추가

---
+++ src/components/Loading.tsx
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled';
+
+const Container = styled.div`
+  width: 100%;
+`;
+
+const Loading = () => {
+  return (
+    <Container>
+      <h1>Loading...</h1>
+    </Container>
+  );
+};
+
+export default Loading;
