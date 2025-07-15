952eb811f72d3a4aaf9055950833a2caf10024ed
dib3474 <dib3474@gmail.com>
2025-07-15 16:08:28 +0900

    chore: dynamic import 적용

--- src/components/routes/Routes.tsx
+++ src/components/routes/Routes.tsx
@@ -1,25 +1,30 @@
+import { Suspense, lazy } from "react";
 import { Route, Routes as RouterRoutes } from "react-router-dom";
-import GiftPage from "@/pages/Gift/GiftPage";
-import LoginPage from "@/pages/Login/LoginPage";
-import NotFoundPage from "@/pages/NotFound/NotFoundPage";
-import ProfilePage from "@/pages/Profile/ProfilePage";
 import AuthRoute from "@/components/routes/AuthRoute";
-import OrderPage from "@/pages/Order/OrderPage";
 import { ROUTE_PATH } from "@/components/routes/routePath";
+
+const GiftPage = lazy(() => import("@/pages/Gift/GiftPage"));
+const LoginPage = lazy(() => import("@/pages/Login/LoginPage"));
+const NotFoundPage = lazy(() => import("@/pages/NotFound/NotFoundPage"));
+const ProfilePage = lazy(() => import("@/pages/Profile/ProfilePage"));
+const OrderPage = lazy(() => import("@/pages/Order/OrderPage"));
+const Loading = lazy(() => import("@/components/common/Loading"));
 
 const Routes = () => {
   return (
-    <RouterRoutes>
-      <Route path={ROUTE_PATH.HOME} element={<GiftPage />} />
-      <Route element={<AuthRoute />}>
-        <Route path={ROUTE_PATH.LOGIN} element={<LoginPage />} />
-      </Route>
-      <Route element={<AuthRoute required />}>
-        <Route path={ROUTE_PATH.PROFILE} element={<ProfilePage />} />
-        <Route path={ROUTE_PATH.ORDER_ID} element={<OrderPage />} />
-      </Route>
-      <Route path={ROUTE_PATH.NOT_FOUND} element={<NotFoundPage />} />
-    </RouterRoutes>
+    <Suspense fallback={<Loading height="100vh" />}>
+      <RouterRoutes>
+        <Route path={ROUTE_PATH.HOME} element={<GiftPage />} />
+        <Route element={<AuthRoute />}>
+          <Route path={ROUTE_PATH.LOGIN} element={<LoginPage />} />
+        </Route>
+        <Route element={<AuthRoute required />}>
+          <Route path={ROUTE_PATH.PROFILE} element={<ProfilePage />} />
+          <Route path={ROUTE_PATH.ORDER_ID} element={<OrderPage />} />
+        </Route>
+        <Route path={ROUTE_PATH.NOT_FOUND} element={<NotFoundPage />} />
+      </RouterRoutes>
+    </Suspense>
   );
 };
 
