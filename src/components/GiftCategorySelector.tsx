46418a12888a414b1dec6217046a27057c05ef9b
anseonghyeon <anseonghyeon@gmail.com>
2025-07-15 20:13:37 +0900

    feat(theme): theme api 연동과 로딩 스피너 추가

--- src/components/GiftCategorySelector.tsx
+++ src/components/GiftCategorySelector.tsx
@@ -2,6 +2,9 @@ import styled from '@emotion/styled';
 import categories from '../mocks/category.mock';
 import PromoBanner from './PromoBanner';
 
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
 const GiftCategorySelectorWrapper = styled.div`
   width: auto;
   height: auto;
@@ -14,7 +17,28 @@ const GiftCategorySelectorTitle = styled.h2`
   line-height: ${({ theme }) => theme.typography.title.title1Bold.lineHeight};
 `;
 
-const GiftCategorySelectorItemBoxStyle = styled.div`
+const GiGiftCategorySelectorItemBoxWrapper = styled.div`
+  width: 100%;
+  height: 300px;
+  display:flex;
+  justify-content: center;
+  align-items: center;
+`
+const Spinner = styled.div`
+  border: 4px solid #f3f3f3;
+  border-top: 4px solid #333;
+  border-radius: 50%;
+  width: 40px;
+  height: 40px;
+  animation: spin 0.8s linear infinite;
+
+  @keyframes spin {
+    0% { transform: rotate(0deg); }
+    100% { transform: rotate(360deg); }
+  }
+`
+
+const GiftCategorySelectorItemBoxGrid = styled.div`
   width: auto;
   height: auto;
   margin-top: ${({ theme }) => theme.spacing.spacing5};
@@ -23,7 +47,7 @@ const GiftCategorySelectorItemBoxStyle = styled.div`
   display: grid;
   grid-template-columns: repeat(5, 1fr);
   row-gap: ${({ theme }) => theme.spacing.spacing5};
-  column-gap: ${({ theme }) => theme.spacing.spacing7};
+  column-gap: ${({ theme }) => theme.spacing.spacing16};
 `;
 
 const GiftCategorySelectorItemWrapper = styled.div`
@@ -40,6 +64,7 @@ const GiftCategorySelectorItemWrapper = styled.div`
 const GiftCategorySelectorItemImg = styled.img`
   width: ${({ theme }) => theme.spacing.spacing13};
   height: ${({ theme }) => theme.spacing.spacing13};
+  border-radius: ${({ theme }) => theme.spacing.spacing4};
 `;
 
 const GiftCategorySelectorItemText = styled.p`
@@ -49,26 +74,50 @@ const GiftCategorySelectorItemText = styled.p`
   margin-top: ${({ theme }) => theme.spacing.spacing1};
 `;
 
+
+
 function GiftCategorySelectorItemBox() {
-  
+  const [themes, setThemes] = useState([]);
+  const [isLoding, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    const fetchThemes = async () => {
+      try {
+        const response = await axios.get('http://localhost:3000/api/themes');
+        setThemes(response.data.data);
+        setIsLoading(false);
+      } catch (error) {
+        console.error('Error fetching weather data:', error);
+        setIsError(true);
+        setIsLoading(false);
+      }
+    };
+
+    fetchThemes();
+  }, []);
+
+  // console.log(themes);
   return (
-    <GiftCategorySelectorItemBoxStyle>
-      {categories.map((item) => (
-        <GiftCategorySelectorItemWrapper key={item.themeId}>
-          <GiftCategorySelectorItemImg
-            src={item.image}
-            alt={item.name}
-          ></GiftCategorySelectorItemImg>
-          <GiftCategorySelectorItemText>
-            {item.name}
-          </GiftCategorySelectorItemText>
-        </GiftCategorySelectorItemWrapper>
-      ))}
-    </GiftCategorySelectorItemBoxStyle>
+    <GiGiftCategorySelectorItemBoxWrapper>
+      {isLoding ? <Spinner></Spinner> : <GiftCategorySelectorItemBoxGrid>
+                {themes.map((item) => (
+                  <GiftCategorySelectorItemWrapper key={item.themeId}>
+                    <GiftCategorySelectorItemImg
+                      src={item.image}
+                      alt={item.name}
+                    ></GiftCategorySelectorItemImg>
+                    <GiftCategorySelectorItemText>
+                      {item.name}
+                    </GiftCategorySelectorItemText>
+                  </GiftCategorySelectorItemWrapper>))}
+      </GiftCategorySelectorItemBoxGrid>}
+    </GiGiftCategorySelectorItemBoxWrapper>
   );
 }
 
 function GiftCategorySelector() {
+
 
   return (
     <GiftCategorySelectorWrapper>
