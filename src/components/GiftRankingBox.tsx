9eaa1103a8d5f9d8efb5735e17953230a2588ecd
aaaaaattt <choigur03@gamil.com>
2025-07-16 02:06:13 +0900

    feat(ranking): API를 사용하여 실시간 급상승 선물 랭킹 섹션 구현

--- src/components/GiftRankingBox.tsx
+++ src/components/GiftRankingBox.tsx
@@ -2,22 +2,64 @@ import type { Theme } from "@emotion/react";
 import { css } from "@emotion/react";
 import GiftObject from "./GiftObject";
 import { useTheme } from "@emotion/react";
-import { giftData } from "@/data/giftData";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const productRankingURL = import.meta.env.VITE_API_BASE_URL_PRODUCT_RANKING;
+
+type ProductRanking = {
+  id: number;
+  name: string;
+  price: {
+    basicPrice: number;
+    sellingPrice: number;
+    discountRate: number;
+  };
+  imageURL: string;
+  brandInfo: {
+    id: number;
+    name: string;
+    imageURL: string;
+  };
+};
 
 const GiftRanking = () => {
   const theme = useTheme();
+  const [productRankingData, setproductRankingData] = useState<
+    ProductRanking[] | null
+  >(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    const fetchProductRanking = async () => {
+      try {
+        const response = await axios.get(productRankingURL);
+        setproductRankingData(response.data.data);
+        setIsLoading(false);
+      } catch (error) {
+        console.error("Error fetching theme data:", error);
+        setIsError(true);
+        setIsLoading(false);
+      }
+    };
+
+    fetchProductRanking();
+  }, []);
+
   const navigate = useNavigate();
   return (
     <div>
       <div css={giftRankingStyle(theme)}>
-        {giftData.map((gift) => (
-          <GiftObject
-            key={gift.id}
-            gift={gift}
-            onClick={() => navigate(`/order/${gift.id}`)}
-          />
-        ))}
+        {productRankingData &&
+          productRankingData.map((product) => (
+            <GiftObject
+              key={product.id}
+              gift={product}
+              onClick={() => navigate(`/order/${product.id}`)}
+            />
+          ))}
       </div>
     </div>
   );
