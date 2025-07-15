fc643438772cec2ec5fcaaf2bec588b3f4f79895
Changmok LEE <cmlee5075@gmail.com>
2025-07-15 20:11:34 +0900

    refactor: API 모듈 구조 리팩토링
       - 상품(랭킹) 및 테마 API 모듈 분리
       - GiftRankingSection, GiftThemeSection 컴포넌트의 API 연동 코드 수정

---
+++ src/api/themes/themeApi.ts
@@ -0,0 +1,39 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { API_BASE_URL } from '../apiBaseUrl';
+
+interface ThemeInfo {
+  themeId: number;
+  name: string;
+  image: string;
+}
+
+interface ThemesResponse {
+  data: ThemeInfo[];
+}
+
+const ThemeApi = () => {
+  const [themes, setThemes] = useState<ThemeInfo[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get<ThemesResponse>(`${API_BASE_URL}/api/themes`);
+        const { data } = response;
+        setThemes(data.data);
+      } catch (error) {
+        setError(true);
+        console.error('Error fetching themes:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return { themes, loading, error };
+};
+
+export default ThemeApi;
