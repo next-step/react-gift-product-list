eb302f7136092199d57d201c22803cc6416354b5
kimdowan <kimdowan1004@naver.com>
2025-07-15 14:31:47 +0900

    fix: 파일구조 이상 발견, 원상태로 복귀

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { ThemeProvider } from '@emotion/react';
+import { theme } from '@/styles/theme';
+import { GlobalStyles } from '@/styles/global';
+import { RouterProvider } from 'react-router-dom';
+import { router } from '@/routes';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </StrictMode>,
-)
+);
