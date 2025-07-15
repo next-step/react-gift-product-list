826f5d9ec078a12d0d948e4dda4c2ed7c7922c0d
kimgho <kgh67811@knu.ac.kr>
2025-07-16 02:27:06 +0900

    feat(themes): 테마 가져오기 api 추가

---
+++ src/api/themes/get-themes.ts
@@ -0,0 +1,15 @@
+import { api } from "@/api/api";
+import ApiErrorHandler from "@/api/ErrorHandler";
+
+interface GetThemesResponseBody {
+  themeId: number;
+  name: string;
+  image: string;
+}
+export const getThemes = async (): Promise<GetThemesResponseBody[]> => {
+  return ApiErrorHandler.executeApi(async () => {
+    const { data: response } =
+      await api.get<BaseResponse<GetThemesResponseBody[]>>("/themes");
+    return response.data;
+  }, "테마를 불러오는 중 오류가 발생했습니다.");
+};
