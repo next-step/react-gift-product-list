49f0c2b119846a452a76782f6581d2664929f9be
Taewoong Heo <chamjoeun0111@gmail.com>
2025-07-15 14:09:12 +0900

    feat(TrendingGift): 빈 데이터 문구 UI 구현

--- src/pages/HomePage/components/TrendingGifts/ProductGrid/ProductGrid.tsx
+++ src/pages/HomePage/components/TrendingGifts/ProductGrid/ProductGrid.tsx
@@ -1,6 +1,8 @@
 import styled from "@emotion/styled";
 import type { TrendingGiftsType } from "@/types/TrendingGiftsType";
 import ProductCard from "../ProductCard/ProductCard";
+import { MoreInfo, MoreInfoWrapper } from "../TrendingGifts.styles";
+import { TRENDING_GIFTS_LABELS } from "../constants/tabs";
 
 const ProductGridContainer = styled.div`
   width: 95%;
@@ -14,21 +16,49 @@ interface ProductGridPropsType {
   products: TrendingGiftsType[];
 }
 
+const EmptyProductContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 125px;
+  margin-bottom: 125px;
+`;
+
+const EmptyProductText = styled.p`
+  font-size: ${({ theme }) => theme.typography.label.label1Regular.fontSize};
+  font-weight: ${({ theme }) =>
+    theme.typography.label.label1Regular.fontWeight};
+`;
+
 function ProductGrid({ products }: ProductGridPropsType) {
+  if (products.length === 0)
+    return (
+      <EmptyProductContainer>
+        <EmptyProductText>상품이 없습니다.</EmptyProductText>
+      </EmptyProductContainer>
+    );
+
   return (
-    <ProductGridContainer>
-      {products.map((product, idx) => (
-        <ProductCard
-          key={product.id}
-          id={product.id}
-          imageURL={product.imageURL}
-          name={product.name}
-          brandName={product.brandInfo.name}
-          sellingPrice={product.price.sellingPrice}
-          index={idx}
-        />
-      ))}
-    </ProductGridContainer>
+    <>
+      <ProductGridContainer>
+        {products.map((product, idx) => (
+          <ProductCard
+            key={product.id}
+            id={product.id}
+            imageURL={product.imageURL}
+            name={product.name}
+            brandName={product.brandInfo.name}
+            sellingPrice={product.price.sellingPrice}
+            index={idx}
+          />
+        ))}
+      </ProductGridContainer>
+      <MoreInfoWrapper>
+        <MoreInfo>{TRENDING_GIFTS_LABELS.MORE_INFO}</MoreInfo>
+      </MoreInfoWrapper>
+    </>
   );
 }
 
