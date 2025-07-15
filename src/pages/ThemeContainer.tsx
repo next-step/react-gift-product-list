50f15f994643a860271ffc7ddf7d15c9f3bfa8fd
HanseKim <kimbird2002@naver.com>
2025-07-15 11:04:35 +0900

    feat: api 연결

--- src/pages/ThemeContainer.tsx
+++ src/pages/ThemeContainer.tsx
@@ -1,12 +1,41 @@
+import { useState, useEffect } from 'react';
 import ThemeItem from '@/components/ThemeItem';
-import mockTheme from '@/mocks/mockdata.ts';
+import type { Themetype } from '@/types/themeDTO/theme';
 import { ThemeContainerWrapper, ThemeTitle, Message } from '@/styles/Theme/ThemeContainer.styles';
+import { getThemes } from '@/apis/theme';
+import axios from 'axios';
 
 function ThemeContainer() {
+  const [themes, setTheme] = useState<Themetype[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string>('');
+
+  useEffect(() => {
+    const getTheme = async () => {
+      try {
+        const data = await getThemes();
+        setTheme(data);
+      } catch (err: unknown) {
+        if (axios.isAxiosError(err)) {
+          const status = err.response?.data?.data?.status || '에러 발생';
+          setError(status);
+        } else {
+          setError('알 수 없는 에러');
+        }
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    getTheme();
+  }, []);
+
+  if (isLoading) return <>테마를 부르는 중입니다.</>;
+  if (error) return <p>{error}</p>;
+
   return (
     <ThemeContainerWrapper>
       <ThemeTitle>선물 테마</ThemeTitle>
-      {mockTheme.map((theme) => (
+      {themes.map((theme) => (
         <ThemeItem key={theme.themeId} theme={theme} />
       ))}
       <Message>응원 메시지!~!</Message>
