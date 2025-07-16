da00c08f4dc3970a8c9f328d9577227a0b85da96
aaaaaattt <choigur03@gamil.com>
2025-07-17 08:00:31 +0900

    feat: userInfo storage 수정 - sessionStrage에서 값 받아오도록 수정 - 직렬화된 값 불러와서 공유 되도록

--- src/context/UserInfoProvider.tsx
+++ src/context/UserInfoProvider.tsx
@@ -1,9 +1,24 @@
-import { useState, type ReactNode } from "react";
+import { useEffect, useState, type ReactNode } from "react";
 import { UserInfoContext } from "@/context/UserInfoContext";
 import type { User } from "@/context/UserInfoContext";
 
 export function UserInfoProvider({ children }: { children: ReactNode }) {
-  const [user, setUser] = useState<User | null>(null);
+  const [user, setUser] = useState<User | null>(() => {
+    const s = sessionStorage.getItem("userInfo");
+    if (s) {
+      return JSON.parse(s);
+    } else {
+      return null;
+    }
+  });
+  useEffect(() => {
+    if (user) {
+      sessionStorage.setItem("userInfo", JSON.stringify(user));
+    } else {
+      sessionStorage.removeItem("userInfo");
+    }
+  }, [user]);
+
   return (
     <UserInfoContext.Provider value={{ user, setUser }}>
       {children}
