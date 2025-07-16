18ad5b3118e44c02671423158cf84d3e817dfab6
kimdowan <kimdowan1004@naver.com>
2025-07-16 16:10:28 +0900

    Logincontext UserID->UserInfo 변경에 따른 MyPage 로직 변경

--- src/pages/MyPage/index.tsx
+++ src/pages/MyPage/index.tsx
@@ -4,27 +4,27 @@ import { PATH } from '@/constants/paths';
 import { Button, Container, Msg, Title } from './styles';
 
 const MyPage = () => {
-  const { logout, userId } = useLogin();
-  const navigate = useNavigate();
+    const { logout, userInfo } = useLogin();
+    const navigate = useNavigate();
 
-  const handleLogout = () => {
-    logout();
-    navigate(PATH.HOME, { replace: true });
-  };
+    const handleLogout = () => {
+        logout();
+        navigate(PATH.HOME, { replace: true });
+    };
 
-  const username = userId ? userId.split('@')[0] : '';
+    const username = userInfo ? userInfo.email.split('@')[0] : '';
 
-  return (
-    <Container>
-      <Title>마이페이지</Title>
-      {userId && (
-        <>
-          <Msg>{username}님 안녕하세요!</Msg>
-          <Msg>이메일 주소는 {userId} 입니다.</Msg>
-        </>
-      )}
-      <Button onClick={handleLogout}>로그아웃</Button>
-    </Container>
-  );
+    return (
+        <Container>
+            <Title>마이페이지</Title>
+            {userInfo && (
+                <>
+                    <Msg>{username}님 안녕하세요!</Msg>
+                    <Msg>이메일 주소는 {userInfo.email} 입니다.</Msg>
+                </>
+            )}
+            <Button onClick={handleLogout}>로그아웃</Button>
+        </Container>
+    );
 };
 export default MyPage;
