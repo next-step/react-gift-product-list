1590a61c5bb371e36ecad33509439946bcb81797
Jaeho <sjaeho348@gmail.com>
2025-07-16 20:13:19 +0900

    refactor :  api 공용 모듈을 분리

--- src/lib/api/rankingProducts.ts
+++ src/lib/api/rankingProducts.ts
@@ -1,18 +1,6 @@
-import axios from "axios";
-import { type Theme, type TargetType, type RankType, type RankingProduct} from "../types/api";
+import { api } from "./core";
+import { type RankType, type TargetType, type RankingProduct } from "../../types/api";
 
-export const api = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL
-});
-
-export const getThemes = async (): Promise<Theme[]> => {
-    try {
-        const response = await api.get<{data: Theme[]}>("/themes");
-        return response.data.data;
-    } catch (error) {
-        throw error;
-    }
-}
 
 export const getRankingProducts = async (targetType: TargetType = 'ALL', rankType: RankType = 'MANY_WISH'): Promise<RankingProduct[]> => {
     try {
