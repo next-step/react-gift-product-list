4292c6438fa761530cb4a5f417f5412eec118819
jiwonWP <151699607+jiwonWP@users.noreply.github.com>
2025-07-16 14:33:57 +0900

    timeout 시간 변경
    Co-authored-by: JO YUN HO <yujo.dev@gmail.com>

--- src/api/client.ts
+++ src/api/client.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 
 export const client = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
-  timeout: 5000,
+  timeout: 5 * 1_000,
   headers: {
     "Content-Type": "application/json",
   },
