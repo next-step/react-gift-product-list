7af876c26af1cba3b8d8f8525f92c716b89c4a62
kimgho <kgh67811@knu.ac.kr>
2025-07-18 02:27:37 +0900

    feat(api): axios 인터셉터 추가

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,3 +1,4 @@
+import { getUserInfo, showToast } from "@/utils";
 import axios from "axios";
 
 export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL as string;
@@ -6,3 +7,36 @@ export const api = axios.create({
   baseURL: API_BASE_URL,
   timeout: 10000,
 });
+
+api.interceptors.request.use(
+  config => {
+    const userInfo = getUserInfo();
+    if (userInfo?.authToken) {
+      config.headers.Authorization = userInfo.authToken;
+    }
+
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  },
+);
+
+api.interceptors.response.use(
+  response => {
+    return response;
+  },
+  error => {
+    if (error.response?.status === 401) {
+      const authErrorMessage = "로그인이 필요합니다.";
+
+      showToast.error(authErrorMessage);
+
+      const customError = new Error(authErrorMessage);
+      customError.name = "AuthenticationError";
+      return Promise.reject(customError);
+    }
+
+    return Promise.reject(error);
+  },
+);
