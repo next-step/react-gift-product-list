7f7584cb84367a39d2723931ac0f6124f449a21d
toothlessdev <toothless042@gmail.com>
2025-07-17 18:58:09 +0900

    feature: 랭킹 상품 조회 API 연동

---
+++ src/features/ranking/services/getRankingGifts.ts
@@ -0,0 +1,55 @@
+import { useCallback, useEffect } from "react";
+
+import { api } from "@/app/lib/api";
+
+import type { RankTypeQuery } from "@/entities/gift/constants/rankType";
+import type { TargetGroupQuery } from "@/entities/gift/constants/targetType";
+
+import { useHTTP } from "@/shared/hooks/useHTTP";
+import { useQueryParamState } from "@/shared/hooks/useQueryParamState";
+
+export type GetRankingGiftsResponseBody = Array<{
+    id: number;
+    name: string;
+    price: {
+        basicPrice: number;
+        sellingPrice: number;
+        discountRate: number;
+    };
+    imageURL: string;
+    brandInfo: {
+        id: number;
+        name: string;
+        imageURL: string;
+    };
+}>;
+
+export const getRankingGifts = async (targetType: TargetGroupQuery, rankType: RankTypeQuery) => {
+    const { data: response } = await api.get<BaseResponse<GetRankingGiftsResponseBody>>(
+        "/products/ranking",
+        { params: { targetType, rankType } },
+    );
+    return response.data;
+};
+
+export const useRankingGifts = () => {
+    const [targetType] = useQueryParamState<TargetGroupQuery>("targetType", "ALL");
+    const [rankType] = useQueryParamState<RankTypeQuery>("rankType", "MANY_WISH");
+
+    const apiFunction = useCallback(() => {
+        if (!targetType || !rankType) {
+            return getRankingGifts("ALL", "MANY_WISH");
+        }
+        return getRankingGifts(targetType, rankType);
+    }, [targetType, rankType]);
+
+    const { isPending, data, error, send } = useHTTP<void, GetRankingGiftsResponseBody>({
+        apiFunction,
+    });
+
+    useEffect(() => {
+        if (targetType && rankType) send();
+    }, [targetType, rankType, send]);
+
+    return { isPending, data, error, send };
+};
