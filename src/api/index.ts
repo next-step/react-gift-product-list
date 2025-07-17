0cce09c15cf31b0b1dfb1fbe643207b3549c8a97
Jiwon Jeong <81281798+jjw5655@users.noreply.github.com>
2025-07-17 22:55:45 +0900

    refactor: loginApi에서 baseURL 상수 사용하도록 수정

--- src/api/index.ts
+++ src/api/index.ts
@@ -36,7 +36,7 @@ apiClient.interceptors.response.use(
 );
 
 export async function loginApi(email: string, password: string) {
-  const response = await axios.post('http://localhost:3000/api/login', {
+  const response = await axios.post(`${baseURL}/api/login`, {
     email,
     password,
   });
