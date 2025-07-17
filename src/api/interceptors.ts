44272232236ea0132d5cdb01251a9f9c337a0639
jmj <waysoleward01@naver.com>
2025-07-17 15:13:36 +0900

    feat: 인터셉터 수정

--- src/api/interceptors.ts
+++ src/api/interceptors.ts
@@ -1,15 +1,22 @@
 import { type AxiosInstance, AxiosError } from "axios";
 import { HTTP_STATUS } from "@/constants/httpStatus";
-
 import { ERROR_MESSAGES } from "@/constants/messages";
+
+interface ErrorResponseBody {
+  data: {
+    status: string;
+    statusCode: number;
+    message: string;
+  };
+}
 
 export const attachInterceptors = (instance: AxiosInstance) => {
   instance.interceptors.response.use(
     (response) => response.data?.data,
 
-    (error: AxiosError<{ message?: string }>) => {
+    (error: AxiosError<ErrorResponseBody>) => {
       const status = error.response?.status;
-      const message = error.response?.data?.message;
+      const message = error.response?.data?.data?.message;
 
       if (status === HTTP_STATUS.UNAUTHORIZED) {
         window.location.href = "/login";
