d1385b3cfc3d8fe312811958b57a1edc3e576909
sinyeowon <ioohyou@knu.ac.kr>
2025-07-18 02:06:03 +0900

    fix: @kakao.com 이메일만 가능하도록 설정 수정

--- src/hooks/useLoginForm.ts
+++ src/hooks/useLoginForm.ts
@@ -11,8 +11,8 @@ interface UseLoginFormProps {
 
 const validateEmail = (value: string) => {
   if (!value) return 'ID를 입력해주세요.';
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  if (!emailRegex.test(value)) return 'ID는 이메일 형식이어야 해요.';
+  const emailRegex = /^[^\s@]+@kakao\.com$/;
+  if (!emailRegex.test(value)) return '@kakao.com 이메일 주소만 가능합니다.';
   return '';
 };
 
