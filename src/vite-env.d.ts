20ccadae6330cf2dc4dbfe3c15355dc61df83f08
jinhyun <jin71744@gmail.com>
2025-07-17 14:39:21 +0900

    feat: axios 인스턴스 설정
    - axios 인스턴스 생성
    - .env 파일에 API 기본 URL 설정

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_API_BASE_URL: string;
+  // 여기에 다른 환경변수 추가 가능
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
