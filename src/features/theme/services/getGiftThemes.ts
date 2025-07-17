27ee189b6eba3f43218133942349a6aa6ad54937
toothlessdev <toothless042@gmail.com>
2025-07-17 18:24:30 +0900

    feature: 테마 목록조회 API 연동

---
+++ src/features/theme/services/getGiftThemes.ts
@@ -0,0 +1,21 @@
+import { api } from "@/app/lib/api";
+
+import { useHTTP } from "@/shared/hooks/useHTTP";
+
+export type GetThemesResponseBody = Array<{
+    themeId: number;
+    name: string;
+    image: string;
+}>;
+
+export async function getGiftThemes() {
+    const { data: response } = await api.get<BaseResponse<GetThemesResponseBody>>("/themes");
+    return response.data;
+}
+
+export const useGiftThemes = () => {
+    return useHTTP<void, GetThemesResponseBody>({
+        apiFunction: getGiftThemes,
+        sendOnMount: true,
+    });
+};
