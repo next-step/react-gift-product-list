f3fb4c6f123b609c9714c7ffdd19ff74d6533ec8
Injun Jeong <jijun897@naver.com>
2025-07-17 14:31:38 +0900

    feat: useApiRequest 훅을 인자 전달 가능한 공용 형태로 변경

--- src/hooks/useApiRequest.ts
+++ src/hooks/useApiRequest.ts
@@ -1,38 +1,41 @@
 import { useCallback, useEffect, useState } from "react";
 
-type UseApiRequestProps<TData> = {
-  requestFn: () => Promise<TData>;
+type UseApiRequestProps<TData, TArgs extends unknown[] = []> = {
+  requestFn: (...args: TArgs) => Promise<TData>;
   immediate?: boolean;
 };
 
-const useApiRequest = <TData = unknown>({
+const useApiRequest = <TData = unknown, TArgs extends unknown[] = []>({
   requestFn,
   immediate = true,
-}: UseApiRequestProps<TData>) => {
+}: UseApiRequestProps<TData, TArgs>) => {
   const [data, setData] = useState<TData | undefined>(undefined);
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(immediate);
   const [isError, setIsError] = useState(false);
 
-  const fetchData = useCallback(() => {
-    setIsLoading(true);
-    setIsError(false);
-    requestFn()
-      .then(response => {
-        setData(response);
-      })
-      .catch(() => {
-        setIsError(true);
-      })
-      .finally(() => {
-        setIsLoading(false);
-      });
-  }, [requestFn]);
+  const fetchData = useCallback(
+    (...args: TArgs) => {
+      setIsLoading(true);
+      setIsError(false);
+      requestFn(...args)
+        .then(response => {
+          setData(response);
+        })
+        .catch(() => {
+          setIsError(true);
+        })
+        .finally(() => {
+          setIsLoading(false);
+        });
+    },
+    [requestFn],
+  );
 
   useEffect(() => {
     if (immediate) {
-      fetchData();
+      fetchData(...([] as unknown as TArgs));
     }
-  }, [immediate, fetchData]);
+  }, [immediate, fetchData, requestFn]);
 
   return {
     data,
