08716fd0d612e747f2af8e90889dfeeabf9e010d
jmj <waysoleward01@naver.com>
2025-07-17 14:45:43 +0900

    feat: toast 라이브러리 적용

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { Global } from "@emotion/react";
 import { reset } from "@/styles/reset";
 import { globalStyle } from "@/styles/globalStyle";
 import Router from "@/routes/Router";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
@@ -9,6 +11,15 @@ function App() {
       <Global styles={reset} />
       <Global styles={(theme) => globalStyle(theme)} />
       <Router />
+      <ToastContainer
+        position="top-center"
+        autoClose={3000}
+        hideProgressBar
+        pauseOnHover
+        closeOnClick
+        draggable={false}
+        theme="light"
+      />
     </>
   );
 }
