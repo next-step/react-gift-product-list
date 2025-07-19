6f6f070f598370c8cc383d7fb59dcdd4be5a4b33
aaaaaattt <choigur03@gamil.com>
2025-07-19 15:43:41 +0900

    refactor(router): BrowserRouter에서 RouterProvider로 변경

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import "./reset.css";
-import App from "./App.tsx";
 import "pretendard/dist/web/static/pretendard.css";
-import { BrowserRouter } from "react-router-dom";
+import router from "@/Router.tsx";
+import { RouterProvider } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <>
+    <RouterProvider router={router} />
+  </>
 );
