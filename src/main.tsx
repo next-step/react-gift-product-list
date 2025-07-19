f49da4430628ee824251cd86ae5f67127ede3aca
aaaaaattt <choigur03@gamil.com>
2025-07-19 22:12:13 +0900

    chore: router import path 이름 수정

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import "./reset.css";
 import "pretendard/dist/web/static/pretendard.css";
-import router from "@/Router.tsx";
+import router from "@/router";
 import { RouterProvider } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
