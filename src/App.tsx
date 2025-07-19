eae2ccb090f7842a3b9dcdfe7b80479a5e20db3d
aaaaaattt <choigur03@gamil.com>
2025-07-19 15:43:16 +0900

    refactor(router): router.tsx에서 관리하는 component 제거 및 Outlet component 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,50 +2,21 @@ import "@/App.css";
 import "@/components/Main";
 import { theme } from "@/styles/theme.ts";
 import { ThemeProvider } from "@emotion/react";
-import { Routes, Route } from "react-router-dom";
-import Login from "@/components/login/Login";
-import Main from "@/components/Main.tsx";
 import NavigationBar from "@/components/NavigationBar";
-import NotFound from "@/components/NotFound";
 import Layout from "@/styles/Layout.tsx";
-import MyPage from "@/components/MyPage.tsx";
 import { UserInfoProvider } from "@/context/UserInfoProvider";
-import Order from "@/components/order/Order";
-import ProtectedRoute from "@/components/ProtectedRoute";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { ROUTE_PATHS } from "./constants/routePath";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  const { MAIN, LOGIN, MYPAGE, ORDER, NOT_FOUND } = ROUTE_PATHS;
   return (
     <>
       <ThemeProvider theme={theme}>
         <Layout>
-          <NavigationBar />
           <UserInfoProvider>
-            <Routes>
-              <Route path={LOGIN} element={<Login />} />
-              <Route path={MAIN} element={<Main />} />
-              <Route
-                path={MYPAGE}
-                element={
-                  <ProtectedRoute>
-                    <MyPage />
-                  </ProtectedRoute>
-                }
-              />
-              <Route
-                path={ORDER}
-                element={
-                  <ProtectedRoute>
-                    <Order />
-                  </ProtectedRoute>
-                }
-              />
-
-              <Route path={NOT_FOUND} element={<NotFound />} />
-            </Routes>
+            <Outlet />
+            <NavigationBar />
           </UserInfoProvider>
         </Layout>
       </ThemeProvider>
