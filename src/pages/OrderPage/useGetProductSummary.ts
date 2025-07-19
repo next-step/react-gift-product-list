c62d2c0f6f832fff324c5f0435df5e771a83209e
kimdowan <kimdowan1004@naver.com>
2025-07-19 19:43:48 +0900

    remove: 사용하지 않는 import 문 삭제

--- src/pages/OrderPage/useGetProductSummary.ts
+++ src/pages/OrderPage/useGetProductSummary.ts
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useCallback } from 'react';
+import { useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import useApi from '../../apis/useApi';
