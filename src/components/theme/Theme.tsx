5caaea3f9079b9c1ac6c56e9b1742fac8f9a075a
aaaaaattt <choigur03@gamil.com>
2025-07-20 20:43:33 +0900

    feat: /api/themes/:themeId/products API를 사용하여 상품 리스트를 구현

--- src/components/theme/Theme.tsx
+++ src/components/theme/Theme.tsx
@@ -3,10 +3,11 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { ClipLoader } from "react-spinners";
-import type { Theme } from "@emotion/react";
+import theme from "@/styles/theme";
 import { css } from "@emotion/react";
 import { useNavigate } from "react-router-dom";
 import { ROUTE_PATHS } from "@/constants/routePath";
+import type { Theme } from "@emotion/react";
 
 type ThemeInfo = {
   name: string;
@@ -15,10 +16,29 @@ type ThemeInfo = {
   backgroundColor: string;
 };
 
+type ThemeProducts = {
+  list: Array<{
+    id: number;
+    name: string;
+    price: {
+      basicPrice: number;
+      sellingPrice: number;
+      discountRate: number;
+    };
+    imageURL: string;
+    brandInfo: {
+      id: number;
+      name: string;
+      imageURL: string;
+    };
+  }>;
+};
+
 const Theme = () => {
   const { themeId } = useParams();
   const { fetchData } = useRequestHandler();
   const [themeInfo, setThemeInfo] = useState<ThemeInfo | null>(null);
+  const [productInfo, setProductInfo] = useState<ThemeProducts | null>(null);
   const navigate = useNavigate();
   const { MAIN } = ROUTE_PATHS;
 
@@ -38,9 +58,19 @@ const Theme = () => {
         }
       },
     });
+
+    fetchData({
+      fetcher: () =>
+        axios.get(
+          `${import.meta.env.VITE_API_BASE_URL_THEME}/${themeId}/products `
+        ),
+      onSuccess: (data) => {
+        setProductInfo(data.data.data);
+      },
+    });
   }, [themeId]);
 
-  if (!themeInfo) {
+  if (!themeInfo || !productInfo) {
     return (
       <div css={spinnerWrapperStyle}>
         <ClipLoader color="#333" size={40} />
@@ -48,16 +78,38 @@ const Theme = () => {
     );
   }
 
+  console.log("productInfo", productInfo);
+
   return (
-    <div css={themeHeroAreaStyle(themeInfo.backgroundColor)}>
-      <h3>{themeInfo.name}</h3>
-      <h2>{themeInfo.title}</h2>
-      <p>{themeInfo.description}</p>
+    <div>
+      <div css={themeHeroAreaStyle(themeInfo.backgroundColor)}>
+        <h3>{themeInfo.name}</h3>
+        <h2>{themeInfo.title}</h2>
+        <p>{themeInfo.description}</p>
+      </div>
+      <div css={themeProductListStyle(theme)}>
+        {productInfo.list.map((product) => (
+          <div key={product.id}>
+            <img src={product.imageURL} alt={product.name} />
+            <h4 css={brandTextStyle(theme)}>{product.brandInfo.name}</h4>
+            <h4 css={productNameStyle(theme)}>{product.name}</h4>
+            <strong css={priceTextStyle(theme)}>
+              {product.price.sellingPrice}원
+            </strong>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
 
 export default Theme;
+
+const themeHeroAreaStyle = (backgroundColor: string) => css`
+  background-color: ${backgroundColor};
+  padding: 20px;
+  color: #fff;
+`;
 
 const spinnerWrapperStyle = css`
   grid-column: 1 / -1;
@@ -67,8 +119,36 @@ const spinnerWrapperStyle = css`
   min-height: 200px;
 `;
 
-const themeHeroAreaStyle = (backgroundColor: string) => css`
-  background-color: ${backgroundColor};
-  padding: 20px;
-  color: #fff;
+const themeProductListStyle = (theme: Theme) => css`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: ${theme.spacing.spacing4};
+  padding: ${theme.spacing.spacing4};
+
+  img {
+    width: 100%;
+    height: auto;
+    border-radius: 8px;
+  }
+`;
+
+const brandTextStyle = (theme: Theme) => css`
+  font-size: ${theme.typography.label1Regular.size};
+  font-weight: ${theme.typography.label1Regular.weight};
+  line-height: ${theme.typography.label1Regular.lineHeight};
+  color: ${theme.colors.semantic.text.sub};
+`;
+
+const productNameStyle = (theme: Theme) => css`
+  font-size: ${theme.typography.body1Regular.size};
+  font-weight: ${theme.typography.body1Regular.weight};
+  line-height: ${theme.typography.body1Regular.lineHeight};
+  color: ${theme.colors.semantic.text.default};
+`;
+
+const priceTextStyle = (theme: Theme) => css`
+  font-size: ${theme.typography.body1Bold.size};
+  font-weight: ${theme.typography.body1Bold.weight};
+  line-height: ${theme.typography.body1Bold.lineHeight};
+  color: ${theme.colors.gray.gray1000};
 `;
