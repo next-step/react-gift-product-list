2c4598bf710f9f6c2b04ad2494ff159c86bcea04
jskim <jskim3936@naver.com>
2025-07-20 16:19:24 +0900

    feat(api): postLogin 추가

--- src/Api/api.ts
+++ src/Api/api.ts
@@ -3,3 +3,7 @@ import axios from 'axios';
 export const api = axios.create({
   baseURL: import.meta.env.VITE_BASE_API_URL,
 });
+
+export const postLogin = (email: string, password: string) => {
+  api.post('/api/login', { email, password });
+};
