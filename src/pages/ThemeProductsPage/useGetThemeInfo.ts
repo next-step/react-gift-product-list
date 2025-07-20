1fafe9161b91e97c89ecc97cdd177e5e1efcda43
kimdowan <kimdowan1004@naver.com>
2025-07-20 22:46:38 +0900

    refactor: theme Info 404에러시 홈으로 이동

--- src/pages/ThemeProductsPage/useGetThemeInfo.ts
+++ src/pages/ThemeProductsPage/useGetThemeInfo.ts
@@ -1,4 +1,9 @@
 import useApi from '@/apis/useApi';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { isAxiosError } from 'axios';
+import { PATH } from '@/constants/paths';
 import { API_URLS } from './constants';
 
 interface ThemeInfo {
@@ -13,6 +18,14 @@ export const useGetThemeInfo = (themeId: number) => {
   const { data, isLoading, error } = useApi<{ data: ThemeInfo }>(
     'get', API_URLS.THEME_INFO(themeId),
   );
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isAxiosError(error) && error.response?.status === 404) {
+      navigate(PATH.HOME);
+    }
+  }, [error, navigate]);
 
   return { themeInfo: data?.data, isLoading, error };
 };
