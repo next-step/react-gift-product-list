38e234d7554310f76b0810669c7997565aeed053
aaaaaattt <choigur03@gamil.com>
2025-07-20 19:57:38 +0900

    feat: API를 사용하여 선물 테마 섹션의 히어로 영역을 구현 - 로딩중일 때 react Spinner를 활용하여 로딩 대기 표시 추가

--- src/components/theme/Theme.tsx
+++ src/components/theme/Theme.tsx
@@ -1,5 +1,66 @@
+import { useRequestHandler } from "@/hooks/useRequestHandler";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { ClipLoader } from "react-spinners";
+import type { Theme } from "@emotion/react";
+import { css } from "@emotion/react";
+
+type ThemeInfo = {
+  name: string;
+  title: string;
+  description: string;
+  backgroundColor: string;
+};
+
 const Theme = () => {
-  return <div>theme 상세 페이지 입니다.</div>;
+  const { themeId } = useParams();
+  const { fetchData } = useRequestHandler();
+  const [themeInfo, setThemeInfo] = useState<ThemeInfo | null>(null);
+
+  useEffect(() => {
+    fetchData({
+      fetcher: () =>
+        axios.get(`${import.meta.env.VITE_API_BASE_URL_THEME}/${themeId}/info`),
+      onSuccess: (data) => {
+        setThemeInfo(data.data.data);
+        console.log("Theme data fetched successfully:", data.data.data);
+      },
+      onError: (error) => {
+        console.error("Error fetching theme data:", error);
+      },
+    });
+  }, [themeId]);
+
+  if (!themeInfo) {
+    return (
+      <div css={spinnerWrapperStyle}>
+        <ClipLoader color="#333" size={40} />
+      </div>
+    );
+  }
+
+  return (
+    <div css={themeHeroAreaStyle(themeInfo.backgroundColor)}>
+      <h3>{themeInfo.name}</h3>
+      <h2>{themeInfo.title}</h2>
+      <p>{themeInfo.description}</p>
+    </div>
+  );
 };
 
 export default Theme;
+
+const spinnerWrapperStyle = css`
+  grid-column: 1 / -1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 200px;
+`;
+
+const themeHeroAreaStyle = (backgroundColor: string) => css`
+  background-color: ${backgroundColor};
+  padding: 20px;
+  color: #fff;
+`;
