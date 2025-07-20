481fc60b42d049f0b61ef50b2e667d3a33547366
kimgho <kgh67811@knu.ac.kr>
2025-07-20 21:34:40 +0900

    fix(order): 중복된 로직 제거

--- src/hooks/order/useOrderCalculation.ts
+++ src/hooks/order/useOrderCalculation.ts
@@ -6,19 +6,6 @@ export const useOrderCalculation = () => {
   const product = watch("product");
   const receivers = watch("receivers");
 
-  const totalPrice = useMemo(() => {
-    if (!product || !receivers || receivers.length === 0) {
-      return 0;
-    }
-
-    const productPrice = product.price.sellingPrice || 0;
-    const totalQuantity = receivers.reduce((total, receiver) => {
-      return total + (receiver.quantity || 0);
-    }, 0);
-
-    return productPrice * totalQuantity;
-  }, [product, receivers]);
-
   const totalQuantity = useMemo(() => {
     if (!receivers || receivers.length === 0) {
       return 0;
@@ -28,6 +15,16 @@ export const useOrderCalculation = () => {
       return total + (receiver.quantity || 0);
     }, 0);
   }, [receivers]);
+
+  const totalPrice = useMemo(() => {
+    if (!product || !receivers || receivers.length === 0) {
+      return 0;
+    }
+
+    const productPrice = product.price.sellingPrice || 0;
+
+    return productPrice * totalQuantity;
+  }, [product, totalQuantity, receivers]);
 
   return {
     totalPrice,
