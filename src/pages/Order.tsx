3de22446dadf473e49cbc4e04fa4f1e28bb15d50
sumin <ssumin512@gmail.com>
2025-07-21 00:48:36 +0900

    refactor: GiftSender,GiftForm 두 개 이름을 GiftForm으로 통일

--- src/pages/Order.tsx
+++ src/pages/Order.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import Layout from '@/components/layout/Layout';
 import NavigationBar from '@/components/navigation-bar/NavigationBar';
 import CardSelect from '@/components/order/CardSelect';
-import GiftSender from '@/components/order/OrderInfo';
+import GiftForm from '@/components/order/OrderInfo';
 
 const Order = () => {
   const [templateMessage, setTemplateMessage] = useState('');
@@ -11,7 +11,7 @@ const Order = () => {
     <Layout>
       <NavigationBar />
       <CardSelect setTemplateMessage={setTemplateMessage} />
-      <GiftSender templateMessage={templateMessage} />
+      <GiftForm templateMessage={templateMessage} />
     </Layout>
   );
 };
