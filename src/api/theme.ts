9bf943f3dfba2afba1dea054840324f9c9b7bc43
jmj <waysoleward01@naver.com>
2025-07-21 13:43:26 +0900

    feat: theme api 추가

--- src/api/theme.ts
+++ src/api/theme.ts
@@ -6,6 +6,50 @@ export type Theme = {
   image: string;
 };
 
+export type ThemeInfo = {
+  themeId: number;
+  name: string;
+  title: string;
+  description: string;
+  backgroundColor: string;
+};
+
+export type Product = {
+  id: number;
+  name: string;
+  price: {
+    basicPrice: number;
+    sellingPrice: number;
+    discountRate: number;
+  };
+  imageURL: string;
+  brandInfo: {
+    id: number;
+    name: string;
+    imageURL: string;
+  };
+};
+
+export type ThemeProductResponse = {
+  list: Product[];
+  cursor: number;
+  hasMoreList: boolean;
+};
+
 export const fetchThemes = async (): Promise<Theme[]> => {
   return await apiClient.get("/themes");
+};
+
+export const fetchThemeInfo = async (themeId: number): Promise<ThemeInfo> => {
+  return await apiClient.get(`/themes/${themeId}/info`);
+};
+
+export const fetchThemeProducts = async (
+  themeId: number,
+  cursor = 0,
+  limit = 10,
+): Promise<ThemeProductResponse> => {
+  return await apiClient.get(`/themes/${themeId}/products`, {
+    params: { cursor, limit },
+  });
 };
