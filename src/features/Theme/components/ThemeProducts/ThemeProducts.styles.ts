cc96be444553b656ad3cd226be8995ef11285ad3
daaoooy <y020d226y@daum.net>
2025-07-21 17:25:48 +0900

    feat(theme-product) : 테마 상품 목록 페이지 상품 리스트 영역 구현

---
+++ src/features/Theme/components/ThemeProducts/ThemeProducts.styles.ts
@@ -0,0 +1,25 @@
+import styled from '@emotion/styled'
+import { theme } from '@/theme'
+
+export const Container = styled.div`
+  background: ${theme.colors.semanticColor.backgroundColor.default};
+  margin-top: ${theme.spacing[7]};
+`
+export const NoProduct = styled.div`
+  height: 200px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  ${theme.typography.body2Regular};
+  text-align: center;
+`
+
+export const ErrorText = styled.div`
+  height: 200px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  ${theme.typography.body2Regular};
+  color: ${theme.colors.semanticColor.stateColor.critical};
+  text-align: center;
+`
---
+++ src/features/Theme/components/ThemeProducts/ThemeProducts.tsx
@@ -0,0 +1,41 @@
+import ProductCard from '@/component/ProductCard/ProductCard'
+import Loading from '@/component/Loading/Loading'
+import { useThemeProducts } from '../../hooks/useThemeProducts'
+import { useInfiniteScroll } from '@/hooks/useInfiniteScroll'
+import * as S from './ThemeProducts.styles'
+
+interface ThemeProductsProps {
+  themeId: number
+  onProductSelect: (product: any) => void
+}
+
+const ThemeProducts = ({ themeId, onProductSelect }: ThemeProductsProps) => {
+  const {
+    products,
+    loading: productsLoading,
+    error: productsError,
+    fetchNextPage,
+    hasMore,
+  } = useThemeProducts(themeId)
+
+  const observerRef = useInfiniteScroll(fetchNextPage, hasMore)
+
+  if (productsError) return <S.ErrorText>{productsError}</S.ErrorText>
+  if (productsLoading && products.length === 0) return <Loading />
+  if (!productsLoading && products.length === 0)
+    return <S.NoProduct>상품이 없습니다.</S.NoProduct>
+
+  return (
+    <S.Container>
+      <ProductCard
+        products={products}
+        onProductSelect={onProductSelect}
+        showToggleButton={false}
+        showRank={false}
+      />
+      {hasMore && <div ref={observerRef} />}
+    </S.Container>
+  )
+}
+
+export default ThemeProducts
