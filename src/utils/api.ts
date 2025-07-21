472c4bd5594918a8355941d47c2b3eeaa9645937
anseonghyeon <anseonghyeon@gmail.com>
2025-07-21 17:04:19 +0900

    feat(theme): 테마정보 요청 실패시 홈으로 리다렉션 처리

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -9,6 +9,8 @@ export function IsErrorStatus(error: any, msg: string,navigate:any) {
     
     if (error.response.status === 401) {
         navigate('/login');
+    } else if(error.response.status === 404) {
+        navigate('/home');
     } else if (error.response && error.response.status >= 400 && error.response.status < 500) {
         toast.error(msg, {
             position: 'bottom-center',
