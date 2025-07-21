f23b19a0d2c28c3c0981bc83c9262070ecea39b4
SeHigh <sehi0119@naver.com>
2025-07-21 13:08:04 +0900

    refactor(theme): 카테고리 섹션을 요구사항에 따라 테마 섹션으로 명칭 변경 및 추가 리팩토링 진행
    - 컴포넌트 명칭 변경 ('Category' -> 'ThemesSection')
    - theme 도메인의 기능 세분화로 features 로 새롭게 통합관리를 위해 이전
      (components/themes -> features/themes/components)

--- src/features/themes/components/ThemesSection.tsx
+++ src/features/themes/components/ThemesSection.tsx
@@ -6,12 +6,12 @@ import { theme } from '@/styles/theme'
 import styled from '@emotion/styled'
 import { Link } from 'react-router-dom'
 
-// * 카테고리 컴포넌트
-export const Category = () => {
-  const { isLoading, isError, data: categories } = useFetch<Theme[]>(fetchThemes)
+// * 테마(Themes) 컴포넌트
+export const ThemesSection = () => {
+  const { isLoading, isError, data: themes } = useFetch<Theme[]>(fetchThemes)
 
   // * 빈 목록 or 에러 화면
-  if (!isLoading && (isError || !categories || categories.length === 0)) return null
+  if (!isLoading && (isError || !themes || themes.length === 0)) return null
 
   // * 바뀌는 영역 조건부 관리
   let body: React.ReactNode
@@ -25,11 +25,11 @@ export const Category = () => {
   } else {
     body = (
       <SubContainer>
-        {categories?.map((category) => (
-          // 카테고리 아이템
-          <Item to={`/category/${category.themeId}`} key={category.themeId}>
-            <Image src={category.image} alt={category.name} />
-            <span css={theme.typography.label.label2Regular}>{category.name}</span>
+        {themes?.map((themeItem) => (
+          // 테마 아이템
+          <Item to={`/themes/${themeItem.themeId}`} key={themeItem.themeId}>
+            <Image src={themeItem.image} alt={themeItem.name} />
+            <span css={theme.typography.label.label2Regular}>{themeItem.name}</span>
           </Item>
         ))}
       </SubContainer>
@@ -39,7 +39,7 @@ export const Category = () => {
   return (
     // 외부 컨테이너
     <Container>
-      {/* 카테고리 타이틀 */}
+      {/* 테마 타이틀 */}
       <h1 css={theme.typography.title.title1Bold}>선물 테마</h1>
       {/* 조건부 렌더링 */}
       {body}
@@ -47,7 +47,7 @@ export const Category = () => {
   )
 }
 
-// * 카테고리 컨테이너 (section 시맨틱 태그 사용)
+// * 테마 컨테이너 (section 시맨틱 태그 사용)
 const Container = styled.section`
   width: 100%;
   height: fit-content;
@@ -61,7 +61,7 @@ const Container = styled.section`
   gap: ${theme.spacing.spacing5};
 `
 
-// * 카테고리 서브 컨테이너
+// * 테마 서브 컨테이너
 const SubContainer = styled.div`
   width: 100%;
   height: fit-content;
@@ -71,7 +71,7 @@ const SubContainer = styled.div`
   gap: ${theme.spacing.spacing5};
 `
 
-// * 카테고리 아이템 - 링크 연결
+// * 테마 아이템 - 링크 연결
 const Item = styled(Link)`
   width: 100%;
   height: 100%;
@@ -83,7 +83,7 @@ const Item = styled(Link)`
   gap: ${theme.spacing.spacing1};
 `
 
-// * 카테고리 이미지
+// * 테마 이미지
 const Image = styled.img`
   width: auto;
   height: 50px;
---
+++ src/features/themes/components/index.ts
@@ -0,0 +1,1 @@
+export { ThemesSection } from './ThemesSection'
