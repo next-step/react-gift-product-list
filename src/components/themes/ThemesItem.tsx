c83f35fffbb96e3e9753f8d6c8c1db18d9dceedf
Injun Jeong <jijun897@naver.com>
2025-07-21 14:21:55 +0900

    feat: 테마 상품 목록 페이지 상품 리스트 구현

---
+++ src/components/themes/ThemesItem.tsx
@@ -0,0 +1,66 @@
+import type { ThemesProduct } from "@/types/theme";
+import styled from "@emotion/styled";
+
+type ThemesItemProps = {
+  product: ThemesProduct;
+};
+
+const ThemesItem = ({ product }: ThemesItemProps) => {
+  return (
+    <Container>
+      <div>
+        <Img src={product.imageURL} alt={product.name} />
+        <BrandName>{product.brandInfo.name}</BrandName>
+        <ProductName>{product.name}</ProductName>
+      </div>
+      <Price>
+        {product.price.sellingPrice} <Span>원</Span>
+      </Price>
+    </Container>
+  );
+};
+
+export default ThemesItem;
+
+const Container = styled.div`
+  display: flex;
+  height: 100%;
+  flex-direction: column;
+  justify-content: space-between;
+  margin: 0;
+`;
+
+const Img = styled.img`
+  width: 100%;
+  height: auto;
+  object-fit: cover;
+  object-position: center center;
+  border-radius: 4px;
+  margin: 0;
+`;
+
+const BrandName = styled.p`
+  font-size: ${({ theme }) => theme.typography.label1Regular.fontSize};
+  font-weight: ${({ theme }) => theme.typography.label1Regular.fontWeight};
+  line-height: ${({ theme }) => theme.typography.label1Regular.lineHeight};
+  color: ${({ theme }) => theme.colors.semantic.text.sub};
+`;
+
+const ProductName = styled.h6`
+  font-size: ${({ theme }) => theme.typography.label1Regular.fontSize};
+  font-weight: ${({ theme }) => theme.typography.label1Regular.fontWeight};
+  line-height: ${({ theme }) => theme.typography.label1Regular.lineHeight};
+  color: ${({ theme }) => theme.colors.semantic.text.default};
+  margin-bottom: ${({ theme }) => theme.spacing.spacing1};
+`;
+
+const Price = styled.p`
+  font-size: ${({ theme }) => theme.typography.title2Bold.fontSize};
+  font-weight: ${({ theme }) => theme.typography.title2Bold.fontWeight};
+  line-height: ${({ theme }) => theme.typography.title2Bold.lineHeight};
+  color: ${({ theme }) => theme.colors.semantic.text.default};
+`;
+
+const Span = styled.span`
+  font-weight: ${({ theme }) => theme.typography.title2Regular.fontWeight};
+`;
--- src/components/themes/ThemesProducts.tsx
+++ src/components/themes/ThemesProducts.tsx
@@ -1,6 +1,10 @@
+import styled from "@emotion/styled";
 import { fetchThemesProducts } from "@/api/themesProducts";
 import useApiRequest from "@/hooks/useApiRequest";
-import { useCallback, useEffect } from "react";
+import { useCallback } from "react";
+import ThemesItem from "./ThemesItem";
+import { Link } from "react-router";
+import { ROUTE_PATH } from "@/routes/paths";
 
 type ThemesProductsProps = {
   id: string | undefined;
@@ -19,23 +23,38 @@ const ThemesProducts = ({ id }: ThemesProductsProps) => {
     requestFn,
   });
 
-  useEffect(() => {
-    if (!themeProductsData && !isLoading && isError) {
-      console.error("Failed to fetch theme products");
-    }
-  }, [themeProductsData, isError, isLoading]);
+  if (!themeProductsData && !isLoading && isError) {
+    console.error("Failed to fetch theme products");
+  }
 
   return (
-    <div>
+    <ProductsSection>
       {themeProductsData && (
-        <ul>
+        <ProductsGrid>
           {themeProductsData.list.map(product => (
-            <li key={product.id}>{product.name}</li>
+            <Link
+              to={ROUTE_PATH.ORDER.replace(":id", String(product.id))}
+              key={product.id}
+            >
+              <ThemesItem product={product} />
+            </Link>
           ))}
-        </ul>
+        </ProductsGrid>
       )}
-    </div>
+    </ProductsSection>
   );
 };
 
 export default ThemesProducts;
+
+const ProductsSection = styled.section`
+  margin: 0;
+  padding: ${({ theme }) => theme.spacing.spacing4};
+  background-color: ${({ theme }) => theme.colors.semantic.background.default};
+`;
+
+const ProductsGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: ${({ theme }) => `${theme.spacing.spacing6} ${theme.spacing.spacing2}`};
+`;
