f4596d2e944ff9a2ff1c308f6fe4fec4a01a9ad3
HanseKim <kimbird2002@naver.com>
2025-07-22 15:03:35 +0900

    feat: 테마 클릭시 상세테마 페이지로 이동

---
+++ src/pages/ThemeDetail.tsx
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled';
+export const ThemeContainerWrapper = styled.div`
+  margin-top: 50px;
+  background-color: #f9f9f9;
+  border-radius: 8px;
+`;
+
+function ThemeDetail() {
+  return <ThemeContainerWrapper>Detail</ThemeContainerWrapper>;
+}
+export default ThemeDetail;
