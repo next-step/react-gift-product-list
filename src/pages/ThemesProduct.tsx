d60b8773afba2fa05e690bdfdb90c2db431b8556
DESKTOP-0FUSSO5\jso51 <leehojun0010@gmail.com>
2025-07-22 17:10:39 +0900

    feat : ThemesProduct page

---
+++ src/pages/ThemesProduct.tsx
@@ -0,0 +1,11 @@
+import NavigationBar from '@src/components/Common/NavigationBar';
+import StyledTopestDiv from '@src/styles/StyledTopesDiv';
+
+const ThemesProduct = () => {
+  return (
+    <StyledTopestDiv>
+      <NavigationBar />
+      <div></div>
+    </StyledTopestDiv>
+  );
+};
