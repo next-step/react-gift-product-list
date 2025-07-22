00dc5ffde8da67644385be194a64961c58c31e65
kimgho <kgh67811@knu.ac.kr>
2025-07-22 17:11:44 +0900

    feat(theme): 테마 페이지 컴포넌트 베럴패턴 적용

---
+++ src/components/themes/index.tsx
@@ -0,0 +1,3 @@
+export { ThemeProductPageLayout } from "@/components/themes/ThemeProductLayout";
+export { ThemeProductHero } from "@/components/themes/ThemeProductHero";
+export { ThemeProductGrid } from "@/components/themes/ThemeProductGrid";
