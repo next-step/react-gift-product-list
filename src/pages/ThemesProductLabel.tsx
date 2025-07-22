4626b5fc7db11d955e51fb6156f77acaf1c13019
DESKTOP-0FUSSO5\jso51 <leehojun0010@gmail.com>
2025-07-22 18:18:19 +0900

    feat : theme item reqeust basic logic

--- src/pages/ThemesProductLabel.tsx
+++ src/pages/ThemesProductLabel.tsx
@@ -1,6 +1,7 @@
 import styled from '@emotion/styled';
 import { apiClient } from '@src/api/FetchData';
 import type { HttpTypes } from '@src/api/HttpType';
+import StyledTopestDiv from '@src/styles/StyledTopesDiv';
 import { useEffect, useState } from 'react';
 
 type ThemeLabel = {
@@ -11,11 +12,34 @@ type ThemeLabel = {
   backgroundColor: string;
 };
 
+type ThemeProducts = {
+  data: {
+    list: ThemeProduct[];
+    cursor: number;
+    hasMoreList: boolean;
+  };
+};
+type ThemeProduct = {
+  id: number;
+  name: string;
+  price: {
+    basicPrice: number;
+    sellingPrice: number;
+    discountRate: number;
+  };
+  imageURL: string;
+  brandInfo: {
+    id: number;
+    name: string;
+    imageURL: string;
+  };
+};
+
 const ThemesProductLabel = () => {
   const urlArray = new URL(window.location.href).pathname.split('/');
   const themeId = urlArray[urlArray.length - 1];
   const [label, setLabel] = useState<ThemeLabel | null>(null);
-  //const [products, setProducts] = useState(null);
+  const [products, setProducts] = useState<ThemeProducts | null>(null);
 
   useEffect(() => {
     const reqeustLabel = async () => {
@@ -35,14 +59,45 @@ const ThemesProductLabel = () => {
       }
     };
     reqeustLabel();
-  }, []);
+  }, [label, themeId]);
+
+  useEffect(() => {
+    const reqeustThemeItem = async () => {
+      const apiReqeustParmas = {
+        methods: 'GET' as HttpTypes,
+        requestName: `themes/${themeId}/products`,
+        body: {},
+        params: '',
+        headers: null,
+      };
+      try {
+        const fetchData = await apiClient(apiReqeustParmas);
+        console.log(fetchData);
+        setProducts(fetchData);
+      } catch (error) {
+        return error;
+      }
+    };
+    reqeustThemeItem();
+  }, [products, themeId]);
 
   return (
-    <StyledThemesProductLabelItem background={label?.backgroundColor}>
-      <p>{label && label.name}</p>
-      <p>{label && label.title}</p>
-      <p>{label && label.description}</p>
-    </StyledThemesProductLabelItem>
+    <StyledTopestDiv>
+      <StyledThemesProductLabelItem background={label?.backgroundColor}>
+        <p>{label && label.name}</p>
+        <p>{label && label.title}</p>
+        <p>{label && label.description}</p>
+      </StyledThemesProductLabelItem>
+      {products &&
+        products.data.list.map((item) => (
+          <div key={item.id}>
+            <img src={item.imageURL} alt='사진 없음' />
+            <p>{item.brandInfo.name}</p>
+            <p>{item.name}</p>
+            <p>{item.price.sellingPrice}</p>
+          </div>
+        ))}
+    </StyledTopestDiv>
   );
 };
 
