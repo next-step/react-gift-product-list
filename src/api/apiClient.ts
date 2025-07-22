5191698c90598c278b80c08b242577ad0d4e1695
Changmok LEE <cmlee5075@gmail.com>
2025-07-22 15:08:22 +0900

    refactor: API_BASE_URL export 수정

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -8,7 +8,7 @@ export interface ApiSuccess<T> {
   message?: string;
 }
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const apiClient: AxiosInstance = axios.create({
   baseURL: `${API_BASE_URL}`,
