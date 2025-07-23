cc4c01e20cc34023440d488656ced162d29848ee
Changmok LEE <cmlee5075@gmail.com>
2025-07-23 15:02:38 +0900

    feat: 404에러 발생 시 홈으로 이동 구현

--- src/page/Themes/hooks/useThemes.ts
+++ src/page/Themes/hooks/useThemes.ts
@@ -1,37 +1,37 @@
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import type { ThemeIdInfoData, ThemeIdProductsData } from '..';
 import axios from 'axios';
-import { toast } from 'react-toastify';
 import { requests } from '@/api/requests';
+import { ROUTES } from '@/routes/Routes';
 
 const useThemes = () => {
   const { id } = useParams<{ id: string }>();
+  const index = Number(id);
+  const navigate = useNavigate();
 
   const [themeIdInfo, setThemeIdInfo] = useState<ThemeIdInfoData>();
   const [themeIdProducts, setThemeIdProducts] = useState<ThemeIdProductsData>();
 
   useEffect(() => {
-    if (!id) return;
+    if (!index) return;
     const fetchData = async () => {
       try {
-        const infoResponse = await requests.fetchThemeIdInfo(Number(id));
-        const productsResponse = await requests.fetchThemeIdProducts(Number(id));
+        const infoResponse = await requests.fetchThemeIdInfo(index);
+        const productsResponse = await requests.fetchThemeIdProducts(index);
         setThemeIdInfo(infoResponse);
         setThemeIdProducts(productsResponse);
       } catch (error) {
         if (axios.isAxiosError(error)) {
-          const status = error.response?.data?.data?.statusCode;
-          toast(
-            status && status === 404
-              ? error.response?.data?.data?.message
-              : '기타 에러 발생(서버 에러, 네트워크 에러 등)'
-          );
+          const status = error.response?.status;
+          if (status === 404) {
+            navigate(ROUTES.HOME);
+          }
         }
       }
     };
     fetchData();
-  }, [id]);
+  }, [index]);
 
   return { themeIdInfo, themeIdProducts };
 };
