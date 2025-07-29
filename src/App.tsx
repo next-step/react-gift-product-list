3781d159944e5ea3f7f93912526cbb40b988487b
ppre1ude <cjh5110@naver.com>
2025-07-29 17:51:32 +0900

    chore: ToastContainer 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { globalStyle } from '@/styles/GlobalStyle';
 import { RouterProvider } from 'react-router-dom';
 import { UserProvider } from '@/contexts/UserContext';
 import Router from '@/routes/Router';
+import { ToastContainer } from 'react-toastify';
 
 const App = () => {
   return (
@@ -11,6 +12,12 @@ const App = () => {
       <UserProvider>
         <RouterProvider router={Router} />
       </UserProvider>
+      <ToastContainer
+        position="bottom-center"
+        autoClose={2000}
+        hideProgressBar={false}
+        closeOnClick
+      />
     </>
   );
 };
