acd64f477c4d77c74fe817b9cf8f20163ad87ac5
whyyhyh <128571447+whyyhyh@users.noreply.github.com>
2025-07-29 17:07:20 +0900

    경북대 FE 남규리-Step2 (#220)
    * feat: 파일 옮겨오기
    
    * feat: 선물하기 홈>선물 테마 섹션 API 이용 테마 목록 완성
    
    * feat: 데이터를 불러오는 동안 로딩화면
    
    * feat: 실시간 급상승 선물랭킹 섹션 API 사용 구현
    
    * feat: userInfo storage 유틸 추가
    
    * fix: 오류 수정
    
    * fix: 로그인 리다이렉트 로직 수정
    
    * fix: 로그인 오류 수정
    
    * fix: 로그인 오류 수정
    
    * fix: 로그인 오류 수정
    
    * feat: 에러 발생시 홈으로 연결
    
    * feat: 401 에러시 로그인 페이지로 연결
    
    * feat: /products/:productId/summary api를 사용
    
    * feat: 인증 헤더 및 에러시 리다이렉트 처리

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,64 @@
-// src/App.tsx
-import { ThemeProvider } from '@emotion/react';
-import { theme } from './styles/theme';
-import { BaseLayout } from './components/Layout/BaseLayout';
-import { Navigation } from './components/Layout/Navigation';
-import { Routes, Route } from 'react-router-dom';
-import HomePage from '@/pages/Home/Page';
-import LoginPage from '@/pages/Login/LoginPage';
-import OrderPage from '@/pages/Home/OrderPage';
-import MyPage from '@/pages/MyPage/MyPage';
-import NotFound from '@/pages/NotFound/Page';
-import { RequireAuth } from '@/components/RequireAuth';
+// // src/App.tsx
+// import { ThemeProvider } from '@emotion/react';
+// import { theme } from './styles/theme';
+// import { BaseLayout } from './components/Layout/BaseLayout';
+// import { Navigation } from './components/Layout/Navigation';
+// import { Routes, Route } from 'react-router-dom';
+// import HomePage from '@/pages/Home/Page';
+// import LoginPage from '@/pages/Login/LoginPage';
+// import OrderPage from '@/pages/Home/OrderPage';
+// import MyPage from '@/pages/MyPage/MyPage';
+// import NotFound from '@/pages/NotFound/Page';
+// import { RequireAuth } from '@/components/RequireAuth';
+// import 'react-toastify/dist/ReactToastify.css'
+// import { ToastContainer } from 'react-toastify'
 
-const App = () => {
+// const App = () => {
+//   return (
+//     <ThemeProvider theme={theme}>
+//       <BaseLayout header={<Navigation />}>
+//         <Routes>
+//           <Route path="/" element={<HomePage />} />
+//           <Route path="/login" element={<LoginPage />} />
+//           <Route path="/order/:id" element={<OrderPage />} />
+
+//           {/* 여기! /my 경로를 RequireAuth로 감싸서 로그인된 사용자만 접근하도록 */}
+//           <Route 
+//             path="/my" 
+//             element={
+//               <RequireAuth>
+//                 <MyPage />
+//               </RequireAuth>
+//             } 
+//           />
+
+//           <Route path="*" element={<NotFound />} />
+//         </Routes>
+//       </BaseLayout>
+//     </ThemeProvider>
+//   );
+// };
+
+// export default App;
+// src/App.tsx
+import React from 'react'
+import { ThemeProvider } from '@emotion/react'
+import { theme } from './styles/theme'
+import { BaseLayout } from './components/Layout/BaseLayout'
+import { Navigation } from './components/Layout/Navigation'
+import { Routes, Route } from 'react-router-dom'
+import HomePage from '@/pages/Home/Page'
+import LoginPage from '@/pages/Login/LoginPage'
+import OrderPage from '@/pages/Home/OrderPage'
+import MyPage from '@/pages/MyPage/MyPage'
+import NotFound from '@/pages/NotFound/Page'
+import { RequireAuth } from '@/components/RequireAuth'
+
+// React Toastify 스타일 & 컨테이너
+import 'react-toastify/dist/ReactToastify.css'
+import { ToastContainer } from 'react-toastify'
+
+const App: React.FC = () => {
   return (
     <ThemeProvider theme={theme}>
       <BaseLayout header={<Navigation />}>
@@ -19,22 +66,32 @@ const App = () => {
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/order/:id" element={<OrderPage />} />
-
-          {/* 여기! /my 경로를 RequireAuth로 감싸서 로그인된 사용자만 접근하도록 */}
-          <Route 
-            path="/my" 
+          <Route
+            path="/my"
             element={
               <RequireAuth>
                 <MyPage />
               </RequireAuth>
-            } 
+            }
           />
-
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BaseLayout>
-    </ThemeProvider>
-  );
-};
 
-export default App;
+      {/* 전역 Toast 컨테이너 */}
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+    </ThemeProvider>
+  )
+}
+
+export default App
---
+++ src/utils/userInfo.ts
@@ -0,0 +1,31 @@
+// src/utils/userInfo.ts
+
+export interface UserInfo {
+  authToken: string;
+  email: string;
+  name: string;
+}
+
+const KEY = 'userInfo';
+
+/**
+ * 로컬스토리지에 UserInfo 저장
+ */
+export function setUserInfo(info: UserInfo) {
+  localStorage.setItem(KEY, JSON.stringify(info));
+}
+
+/**
+ * 저장된 UserInfo 반환 (없으면 null)
+ */
+export function getUserInfo(): UserInfo | null {
+  const json = localStorage.getItem(KEY);
+  return json ? (JSON.parse(json) as UserInfo) : null;
+}
+
+/**
+ * UserInfo 삭제
+ */
+export function clearUserInfo() {
+  localStorage.removeItem(KEY);
+}
